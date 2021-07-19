752a0e9c84f2a9d922e6ff6cbadaf6bf2fe5b677
Matt Henry <matt.henry@gsa.gov>
2021-07-19 12:39:04 -0400

    implement process list

--- src/pages/docket-entry-remand.js
+++ src/pages/docket-entry-remand.js
@@ -5,34 +5,6 @@ import SEO from '../components/seo';
 import Alert from '../components/alert';
 import { Link } from 'gatsby';
 
-const initialState = {
-  caseNumber: '21-12345',
-  caption: 'United States v. Timothy Carpenter',
-  parties: [
-    { partyName: 'United States', partyDesignation: 'Government' },
-    { partyName: 'Timothy Carpenter', partyDesignation: 'Defendant' },
-  ],
-  events: [
-    {
-      date: {
-        month: '09',
-        day: '26',
-        year: '2016',
-      },
-      description: 'Notice of Appeal filed',
-    },
-    {
-      date: {
-        month: '06',
-        day: '30',
-        year: '2021',
-      },
-      description:
-        'Order remanding the case to the district court for further proceedings',
-    },
-  ],
-};
-
 const docketReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_PARTY':
